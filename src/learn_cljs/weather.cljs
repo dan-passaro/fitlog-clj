d42be98ce4201b9485a4e2517e335f0b8c4bf7b8
Dan Passaro <danpassaro@gmail.com>
2026-05-11 02:24:24 -0400

    add workouts junk

--- src/learn_cljs/weather.cljs
+++ src/learn_cljs/weather.cljs
@@ -1,21 +1,35 @@
 (ns learn-cljs.weather
   (:require
    [reagent.core :as reagent :refer [atom]]
+   [reagent.core :as r]
    [reagent.dom.client :as rdc]
    [reagent.dom :as rdom]))
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce click-count (r/atom 0))
+(defonce timer-secs (r/atom 0))
+(defonce timer-timeout nil)
+
+(defn counter []
+  [:p
+   "The atom " [:code "click-count"] " has value: "
+   @click-count ". "
+   [:input {:type "button" :class "btn" :value "Click Me!"
+            :on-click #(swap! click-count inc)}]])
+
+(defn timer []
+  (when (not timer-timeout)
+    (set! timer-timeout (r/atom (js/setInterval #(swap! timer-secs inc) 1000))))
+  [:p "Seconds elapsed: " @timer-secs])
+
+(defn home []
+  [:<>
+   [:h2 {:class "text-2xl"} "Workouts"]
+   [:p {:class "flex justify-center"}
+    [:button {:class "btn btn-primary w-64"}
+     "New Workout"]]])
 
 (defonce root
   (rdc/create-root (.getElementById js/document "app")))
 
-(defn hello-world []
-  [:div
-   [:h1 "Hum: " (:text @app-state)]])
-
-(defn mount []
-  (rdc/render root [hello-world]))
-
 (defn ^:dev/after-load init []
-  (mount))
+  (rdc/render root [home]))
